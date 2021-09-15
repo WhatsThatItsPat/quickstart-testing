39ad18b1cbd6551608cd123e4538f401a5fe9f0c
Patrick McDonald <66495007+eieioxyz@users.noreply.github.com>
2021-09-15 13:44:21 -0400

    Tweak formatting, etc.

--- unit-test-cloud-functions/functions/index.js
+++ unit-test-cloud-functions/functions/index.js
@@ -5,46 +5,54 @@ admin.initializeApp();
 /**
  * Simple HTTP function that returns the "?text" query parameter in the response text.
  */
-exports.simpleHttp = functions.https.onRequest((request, response) => {
-  response.send(`text: ${request.query.text}`);
-});
+exports.simpleHttp = functions.https
+  .onRequest((request, response) => {
+    response.send(`text: ${request.query.text}`);
+  });
 
 /**
  * Simple callable function that adds two numbers.
  */
-exports.simpleCallable = functions.https.onCall((data, ctx) => {
-  // This function implements addition (a + b = c)
-  const sum = data.a + data.b;
-  return {
-    c: sum,
-  };
-});
+exports.simpleCallable = functions.https
+  .onCall((data, context) => {
+    // This function implements addition (a + b = c)
+    const sum = data.a + data.b;
+    return {
+      c: sum,
+    };
+  });
 
 /**
  * Firestore-triggered function which uppercases a string field of a document.
  */
 exports.firestoreUppercase = functions.firestore
   .document("/lowercase/{doc}")
-  .onCreate(async (doc, ctx) => {
+  .onCreate(async (doc, context) => {
     const docId = doc.id;
 
     const docData = doc.data();
     const lowercase = docData.text;
 
-    const firestore = admin.firestore();
-    await firestore.collection("uppercase").doc(docId).set({
-      text: lowercase.toUpperCase(),
-    });
+    const adminDb = admin.firestore();
+    
+    await adminDb
+      .collection("uppercase")
+      .doc(docId)
+      .set({
+        text: lowercase.toUpperCase(),
+      });
   });
 
 /**
  * Auth-triggered function which writes a user document to Firestore.
  */
-exports.userSaver = functions.auth.user().onCreate(async (user, ctx) => {
-  const firestore = admin.firestore();
-  
-  // Make a document in the user's collection with everything we know about the user
-  const userId = user.uid;
-  const userRef = firestore.collection("users").doc(userId);
-  await userRef.set(user.toJSON());
-});
+exports.userSaver = functions.auth.user()
+  .onCreate(async (user, context) => {
+    const adminDb = admin.firestore();
+    
+    // Make a document in the user's collection with everything we know about the user
+    await adminDb
+      .collection("users")
+      .doc(user.uid)
+      .set(user.toJSON());
+  });
